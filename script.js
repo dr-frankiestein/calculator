5de0b66ab0d9557e09ca95802c4f37e59b3d8fda
dr-frankiestein <150306010+dr-frankiestein@users.noreply.github.com>
2024-02-14 18:38:36 -0500

    Make the calculator work
    equals sign must be pressed to trigger evaluation
    of operation. Must add operate function to other
    operator buttons next.

--- script.js
+++ script.js
@@ -8,65 +8,126 @@ let operator;
 let secondNum;
 
 const operate = function(numOne, numTwo, operator) {
+  numOne = Number(numOne);
+  numTwo = Number(numTwo);
   switch(operator) {
-    case '+':
-      add(numOne,numTwo);
+    case "+":
+      return add(numOne,numTwo);
       break;
-    case '-':
-      subtract(numOne,numTwo);
+    case "-":
+      return subtract(numOne,numTwo);
       break;
-    case '*':
-      multiply(numOne,numTwo);
+    case "*":
+      return multiply(numOne,numTwo);
       break;
-    case '/':
-      divide(numOne,numTwo);
+    case "/":
+      return divide(numOne,numTwo);
       break;
     default:
-      // code block
+      return "ERROR";
     } 
 }
 
-const numberBtns = document.querySelectorAll('.number');
+numberOne = document.getElementById('one');
+const originalNumberBG = getComputedStyle(numberOne).backgroundColor;
 
+const numberBtns = document.querySelectorAll('.number');
 numberBtns.forEach(function(number) {
   number.addEventListener("mousedown", function (event) {
     event.target.style.backgroundColor = "hsl(189, 32%, 83%)";
-  });
-  number.addEventListener("mouseup", function(event) {
-    event.target.style.backgroundColor = "hsl(188, 45%, 78%)";
-    clear();
+    setTimeout(function() {
+      event.target.style.backgroundColor = originalNumberBG;
+    }, 200);
   });
 });
+
 
 const operatorBtns = document.querySelectorAll('.operator');
 operatorBtns.forEach(function(operator) {
   operator.addEventListener("mousedown", function (event) {
     event.target.style.backgroundColor = "hsl(46, 74%, 92%)";
+    let opVal = event.target.innerHTML;
+    vals.push(displayValue);
+    vals.push(opVal);
+    clearDisplay(); 
   });
   if (operator.id === "equals") {
     operator.addEventListener("mouseup", (event) => {
-      clear();
+      clearShading();
     });
   };
 });
 
 let displayValue = '';
+let vals = [];
 const displayDiv = document.getElementById("display");
 function display(currentdiv) {
   let divContents = document.getElementById(currentdiv).innerHTML;
-  displayValue = divContents;
+  displayValue += divContents;
   displayDiv.textContent = displayValue;
   }
 
+equalsBtn = document.getElementById("equals");
+equalsBtn.addEventListener("click", () => {
+  vals.push(displayValue);
+  numOne = takeFirstValue(vals);
+  // numOne = vals.splice(0,1);
+  operation = takeFirstValue(vals);
+  numTwo = takeFirstValue(vals);
+  result = operate(numOne, numTwo, operation);
+  displayValue = result;
+  displayDiv.textContent = result;
+  // vals.unshift(result);
+  clearShading();
+})
 
-clearBtn = document.getElementById("clear");
-clearBtn.addEventListener("click", () => {
-  displayValue = '';
-  displayDiv.textContent = 0;
+let originalOpsBG = getComputedStyle(equalsBtn).backgroundColor; // Store original color (red)
+equalsBtn.addEventListener("mousedown", function(event) {
+  event.target.style.backgroundColor = "hsl(46, 74%, 92%)";
+  setTimeout(function(){
+    event.target.style.backgroundColor = originalOpsBG;  // Change the color back to the original
+  }, 200);
 });
 
-clear = function() {
+const miscBtns = document.querySelectorAll('.misc');
+
+miscBtns.forEach(function(misc) {
+  misc.addEventListener("mousedown", function(event) {
+    const originalMiscBG = getComputedStyle(misc).backgroundColor;
+    event.target.style.backgroundColor = "hsl(46, 84%, 86%)";
+    setTimeout(function(){
+      event.target.style.backgroundColor = originalMiscBG;
+    }, 200);
+  });
+});
+
+
+
+
+
+const takeFirstValue = function(array) {
+  lilArray = array.splice(0,1);
+  return lilArray[0];
+};
+
+const clearBtn = document.getElementById("clear");
+clearBtn.addEventListener("click", () => {
+  resetAllVals();
+  clearShading();
+});
+
+const clearShading = function() {
   operatorBtns.forEach(function(operator) {
     operator.style.backgroundColor = "hsl(168, 69%, 84%)";
     });
   };
+
+const clearDisplay = function() {
+  displayValue = '';
+};
+
+const resetAllVals = function() {
+  vals = [];
+  clearDisplay();
+  displayDiv.textContent = 0;
+}

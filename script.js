3c5f360c6bc79fa32a4899a39115bdd708b13318
dr-frankiestein <150306010+dr-frankiestein@users.noreply.github.com>
2024-02-14 14:21:39 -0500

    Code number buttons to populate display

--- script.js
+++ script.js
@@ -25,3 +25,48 @@ const operate = function(numOne, numTwo, operator) {
       // code block
     } 
 }
+
+const numberBtns = document.querySelectorAll('.number');
+
+numberBtns.forEach(function(number) {
+  number.addEventListener("mousedown", function (event) {
+    event.target.style.backgroundColor = "hsl(189, 32%, 83%)";
+  });
+  number.addEventListener("mouseup", function(event) {
+    event.target.style.backgroundColor = "hsl(188, 45%, 78%)";
+    clear();
+  });
+});
+
+const operatorBtns = document.querySelectorAll('.operator');
+operatorBtns.forEach(function(operator) {
+  operator.addEventListener("mousedown", function (event) {
+    event.target.style.backgroundColor = "hsl(46, 74%, 92%)";
+  });
+  if (operator.id === "equals") {
+    operator.addEventListener("mouseup", (event) => {
+      clear();
+    });
+  };
+});
+
+let displayValue = '';
+const displayDiv = document.getElementById("display");
+function display(currentdiv) {
+  let divContents = document.getElementById(currentdiv).innerHTML;
+  displayValue = divContents;
+  displayDiv.textContent = displayValue;
+  }
+
+
+clearBtn = document.getElementById("clear");
+clearBtn.addEventListener("click", () => {
+  displayValue = '';
+  displayDiv.textContent = 0;
+});
+
+clear = function() {
+  operatorBtns.forEach(function(operator) {
+    operator.style.backgroundColor = "hsl(168, 69%, 84%)";
+    });
+  };
